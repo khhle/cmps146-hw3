1403e1bf82c1d919972cab0b2d61bdfe2058a85c
khhle <khhle@ucsc.edu>
2015-04-19 20:52:03 -0700

    Dijkstra and A* finished

--- p3_pathfinder.py
+++ p3_pathfinder.py
@@ -1,5 +1,5 @@
-
-from heapq import heappush, heappop
+import collections
+import heapq
 from math import sqrt
 #It should define a function called "find_path". This function should take three arguments:
 #source_point: (x,y) pair
@@ -14,12 +14,12 @@ from math import sqrt
 def find_path(src_point,dst_point,mesh):
     path = []
     visited_nodes = []
-    coordinates = []
+
     box_list = mesh['boxes']
     adj_list = mesh['adj']
 
-    box_list_traverse = []
-    detail_points = {'Box': box_list_traverse, 'xy': coordinates}
+    detail_points = {}
+
 
     #find box contain src
     for box in box_list:
@@ -32,40 +32,49 @@ def find_path(src_point,dst_point,mesh):
             visited_nodes.append(box)
 
 
-    #bfs_path = bfs(visited_nodes[0],visited_nodes[1],adj_list)
-    bfs_path = dijkstras_shortest_path(visited_nodes[0], src_point, visited_nodes[1],adj_list)
-    for my_path in bfs_path:
+    #found_path,detail_points = bfs(visited_nodes[0],visited_nodes[1],adj_list,src_point)
+    #found_path,detail_points = dijkstra_search(visited_nodes[0],visited_nodes[1],adj_list,src_point)
+    found_path,detail_points = a_star_search(visited_nodes[0],visited_nodes[1],adj_list,src_point,dst_point)
+
+    #Re-construct the path using line segment
+    #start at dst_point since found_path in reverse order
+    pstart = dst_point
+    for my_path in found_path:
+        pend = detail_points[my_path][0]
+        path.append((pstart,pend))
+        pstart = pend
+
+
+    #Add path to visited_nodes
+    for my_path in found_path:
         visited_nodes.append(my_path)
-    path = path_search(bfs_path,src_point, dst_point)
-    #path.append((src_point, dst_point))
-    #path = ((100,200),(300,400))
-    #print str(test)
+
+
     return path,visited_nodes
 
+
+#Calculate distance between 2 points
 def euclidian(src,dst):
     startx,starty = src
     dstx, dsty = dst
 
     return sqrt((startx-dstx)*(startx-dstx) + (starty-dsty)*(starty-dsty))
 
-def find_point(src, box): #finds midpoint in box
+#Find a point inside a given box
+def find_point(src, box):
     x1,x2,y1,y2 = box
     srcx, srcy = src
     dstx = min(x2-1,max(x1,srcx))
     dsty = min(y2-1,max(y1,srcy))
     return dstx, dsty
 
-def path_search(boxes,src, end):
-    path = []
-    for box in boxes:
-        if box == boxes[-1]:
-            coordinate = end
-            path.append((src,coordinate))
-        else:
-            coordinate = find_point(src, box)
-            path.append((src,coordinate))
-            src = coordinate
-    return path
+#Find the cost from one box to another
+def find_cost(src_box,dst_box,src_point):
+    dst_point = find_point(src_point,dst_box)
+    distance = euclidian(src_point, dst_point)
+    return distance,dst_point
+
+
 
 #check if a point is inside a rect
 def isInsideRect(rect,point):
@@ -83,61 +92,14 @@ def isInsideRect(rect,point):
 
     return True
 
-#finds distances from current box to adj boxes
-def dfind_distance(adj, src_point):
-    valid = []
-    for box in adj:
-        point = find_point(src_point, box)
-        distance = euclidian(src_point, point)
-        valid.append((distance, point))
-    return valid
 
 
-def dijkstras_shortest_path(src_box, src_point, dst, adj):
-    Dist = {}
-    Prev = {}
-    #and another one to check if nodes have been fully processed
-    Processed = {}
-    Dist[src_box] = 0
-    queue = [(0,src_box)]
-    nextCells = [(Dist[src_box], src_point)]
-    found = False
 
-    #while queue is not empty. If it's empty we've searched all valid points
-    while(queue):
-        curr = heappop(queue)
-        Processed[curr[1]] = True
-        #print "Popping %s with Distance" %(curr[1],)
 
-        nextCells = dfind_distance(adj, src_box)
-        print (nextCells[0][1])
-        for i in range(0, len(nextCells)):
-            if not (nextCells[i][1] in Processed):
-                #update the distance in the current tuple
-                nextCells[i] = (euclidian, nextCells[i][1])
-                #check if distance needs updating in the dict
-                if not (nextCells[i][1] in Dist) or (Dist[nextCells[i][1]] > nextCells[i][0]):
-                    Dist[nextCells[i][1]] = nextCells[i][0]
-                    Prev[nextCells[i][1]] = curr[1]
-                    #push onto the queue
-                    heappush(queue, nextCells[i])
-                    if nextCells[i][1] == dst:
-                        found = True
-                        break
-        if found:
-            break
-
-    if found:
-        path = []
-        curr = Prev[dst]
-        while curr != src_box:
-            path.append(curr)
-            curr = Prev[curr]
-        return path
 
 
 #BFS search
-def bfs(source, target, adj, verbose=False):
+def bfs(source, target, adj,src_point):
     """Find a path from source to target on the graph defined by the adj function."""
 
     # initialize bookkeeping structures
@@ -145,22 +107,22 @@ def bfs(source, target, adj, verbose=False):
     parent = {}
     discovered = {}
     queue = []
-
+    detail_points = collections.defaultdict(list)
     # explore starting at the source node
 
     discovered[source] = True
     queue.append(source)
 
     iterations = 0
-
+    detail_points[source].append(src_point)
     while queue:
 
         iterations += 1
 
         u = queue.pop()
 
-        if verbose:
-            print "processing", u
+        #if verbose:
+        #    print "processing", u
 
         if u == target: # early termination
             break
@@ -170,9 +132,11 @@ def bfs(source, target, adj, verbose=False):
                 discovered[v] = True
                 parent[v] = u
                 queue.append(v)
+                temp_point = find_point(detail_points[u][0],u)
+                detail_points[v].append(temp_point)
 
-    if verbose:
-        print "terminated after %d iterations" % iterations
+    #if verbose:
+    #    print "terminated after %d iterations" % iterations
 
     # reconstruct the path backwards, starting at the target node
 
@@ -182,9 +146,109 @@ def bfs(source, target, adj, verbose=False):
         path.append(node)
         node = parent[node]
     path.append(source)
-    path.reverse()
+    #path.reverse()
 
-    return path
+    return path,detail_points
 
 
 
+#Dijkstra search
+def dijkstra_search(start, goal,adj,src_point):
+    frontier = PriorityQueue()
+    frontier.put(start, 0)
+    came_from = {}
+    cost_so_far = {}
+    came_from[start] = None
+    cost_so_far[start] = 0
+    detail_points = collections.defaultdict(list)
+
+    detail_points[start].append(src_point)
+
+    while not frontier.empty():
+        current = frontier.get()
+
+        if current == goal:
+            break
+        nextCells = adj[current] #find all the adj boxes
+        for next in nextCells:
+            cost, temp_point = find_cost(current,next,detail_points[current][0]) #find the cost and point inside
+            new_cost = cost_so_far[current] + cost
+            #new_cost = cost_so_far[current] + graph.cost(current, next)
+            if next not in cost_so_far or new_cost < cost_so_far[next]:
+                cost_so_far[next] = new_cost
+                priority = new_cost
+                frontier.put(next, priority)
+                came_from[next] = current
+                #pstart = pend
+                detail_points[next].append(temp_point)
+
+    current = goal
+    path = [current]
+    while current != start:
+        current = came_from[current]
+        path.append(current)
+    return path,detail_points
+
+
+#Helper function for A*
+def heuristic(a, b):
+    (x1, y1) = a
+    (x2, y2) = b
+    return abs(x1 - x2) + abs(y1 - y2)
+
+
+
+
+#A* search
+def a_star_search(start, goal,adj,src_point,dst_point):
+    frontier = PriorityQueue()
+    frontier.put(start, 0)
+    came_from = {}
+    cost_so_far = {}
+    came_from[start] = None
+    cost_so_far[start] = 0
+    detail_points = collections.defaultdict(list)
+
+    detail_points[start].append(src_point)
+
+    while not frontier.empty():
+        current = frontier.get()
+
+        if current == goal:
+            break
+        nextCells = adj[current] #find all the adj boxes
+        for next in nextCells:
+            cost, temp_point = find_cost(current,next,detail_points[current][0]) #find the cost and point inside
+            new_cost = cost_so_far[current] + cost
+            #new_cost = cost_so_far[current] + graph.cost(current, next)
+            if next not in cost_so_far or new_cost < cost_so_far[next]:
+                cost_so_far[next] = new_cost
+                priority = new_cost + euclidian(dst_point, temp_point)
+                #priority = new_cost + heuristic(dst_point, temp_point)
+                frontier.put(next, priority)
+                came_from[next] = current
+                detail_points[next].append(temp_point)
+
+    current = goal
+    path = [current]
+    while current != start:
+        current = came_from[current]
+        path.append(current)
+    return path,detail_points
+
+
+
+
+#Helper class for search function
+class PriorityQueue:
+    def __init__(self):
+        self.elements = []
+
+    def empty(self):
+        return len(self.elements) == 0
+
+    def put(self, item, priority):
+        heapq.heappush(self.elements, (priority, item))
+
+    def get(self):
+        return heapq.heappop(self.elements)[1]
